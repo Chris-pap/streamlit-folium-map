ab6ed52f5737a9402bb8cc2baad61d5b1f78499d
Christos Papadimitriou <72304041+Chris-pap@users.noreply.github.com>
2024-02-20 22:57:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,10 @@ def main():
         .block-container {padding: 0}
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
+        .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+        .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+        .viewerBadge_text__1JaDK {
+        display: none;}
         </style>""",
         unsafe_allow_html=True
     )
